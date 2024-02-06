b7fc49382529ff8b906abc0ec028b1a2625bcdde
Praveen Kumar <praveen@deskfactors.com>
2024-02-06 17:37:41 +0530

    Initial Commit

---
+++ RandomDogGeneratorUITests/RandomDogGeneratorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RandomDogGeneratorUITestsLaunchTests.swift
+//  RandomDogGeneratorUITests
+//
+//  Created by Praveen Kumar on 06/02/24.
+//
+
+import XCTest
+
+final class RandomDogGeneratorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
